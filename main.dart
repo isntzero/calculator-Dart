24e7c065ab770157448b4a44014959bd4d5ab795
isntzero <rmg-51@outlook.com>
2024-05-30 05:01:03 -0600

    Operaciones funcionales con flujo condicional

--- main.dart
+++ main.dart
@@ -27,4 +27,42 @@ void main(){
   //Para escoger la opcion que desea
   stdout.write('Por favor, ingresa la opcion:');
   int opcion = int.parse(stdin.readLineSync()!);
+
+  //Flujo condicional
+  if(opcion == 1){
+    stdout.write('Ingresa el primer numero: ');
+    int num1 = int.parse(stdin.readLineSync()!);
+    stdout.write('Ingresa el segundo numero: ');
+    int num2 = int.parse(stdin.readLineSync()!);
+    int resultado = num1 + num2;
+    print('El resultado es: $resultado'); }
+
+    if(opcion == 2){
+    stdout.write('Ingresa el primer numero: ');
+    int num1 = int.parse(stdin.readLineSync()!);
+    stdout.write('Ingresa el segundo numero: ');
+    int num2 = int.parse(stdin.readLineSync()!);
+    int resultado = num1 - num2;
+    print('El resultado es: $resultado'); }
+
+     if(opcion == 3){
+    stdout.write('Ingresa el primer numero: ');
+    int num1 = int.parse(stdin.readLineSync()!);
+    stdout.write('Ingresa el segundo numero: ');
+    int num2 = int.parse(stdin.readLineSync()!);
+    int resultado = num1 * num2;
+    print('El resultado es: $resultado'); }
+
+    if(opcion == 3){
+    stdout.write('Ingresa el primer numero: ');
+    int num1 = int.parse(stdin.readLineSync()!);
+    stdout.write('Ingresa el segundo numero: ');
+    int num2 = int.parse(stdin.readLineSync()!);
+    double resultado = num1 / num2;
+    print('El resultado es: $resultado'); }
+
+    else {
+      print('Opcion no valida');
+    }
+
 }
